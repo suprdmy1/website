347424ae50b3c1920a21d9bf4dc40b145d34923e
Dmytro Suprun <dmytro.suprun@nagrastar.com>
2021-06-09 14:23:58 -0600

    styling update

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -3,13 +3,9 @@ import "../../App.css";
 import HeroSection from "../HeroSection";
 import Cards from "../Cards";
 
-function Home() {
-    return (
-        <>
-            <HeroSection />
-            <Cards />
-        </>
-    )
+export default function Home() {
+    return (<>
+        <HeroSection />
+        <Cards />
+    </>);
 }
-
-export default Home;
