12d8116658044fb6db1a9f9a80b44cae6cc2621e
Dmytro Suprun <dmytro.suprun@nagrastar.com>
2021-06-09 14:10:34 -0600

    Added draft version of website

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className="footer-container">
+            <section className="footer-subscription">
+                <p className="footer-subscription-heading"></p>
+            </section>
+        </div>
+    );
+}
+
+export default Footer;
